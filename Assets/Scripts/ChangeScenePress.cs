decfca1c3ca90f74e765b897807e44c834c3f5f2
Aaron <aaronfreedman18@gmail.com>
2019-05-14 15:29:01 -0700

    Kept working on start button

---
+++ Assets/Scripts/ChangeScenePress.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenePress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnMouseUp(string CharacterSelect)
+    {
+        SceneManager.LoadScene(CharacterSelect);
+    }
+}
