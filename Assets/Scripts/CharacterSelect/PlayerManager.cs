02d45cbc78bd1551999f6421b8734169042fc6a0
Aaron <aaronfreedman18@gmail.com>
2019-05-29 14:35:11 -0700

    Character Select is being worked on

--- Assets/Scripts/CharacterSelect/PlayerManager.cs
+++ Assets/Scripts/CharacterSelect/PlayerManager.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngineInternal;
 
-public class NewBehaviourScript : MonoBehaviour
+namespace CharacterSelect
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerManager : MonoBehaviour
     {
+        //[SerializeField] private Dropdown playerCountDropdown; //Dropdown for selecting number of players             
         
-    }
+        [SerializeField] private Player[] players; //Main array to hold players
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField] private GameObject tokenSelectionCanvas;//Canvas on which tokens are clicked and selected by players
+
+        private int _playerIndex;
+        
+        //Simply makes it so this game object will not go away, so we can keep accessing array
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        //Initializes the player array in the beginning of the game
+        public void InitializePlayers()
+        {
+            players = new Player[2];
+            for (int n = 0; n < 2; n++)
+            {
+                players[n] = gameObject.AddComponent<Player>();
+            }
+        }
+        
+        //Simply assigns a token to a player in the start screen (triggers from TokenClickDetector)
+        public void AssignToken(Sprite assignedToken)
+        {
+             //Exists to keep track of player that we are assigning token sprite too
+            players[_playerIndex].SetPlayerToken(assignedToken); 
+            _playerIndex += 1;
+            if (_playerIndex < 2)
+            {
+                tokenSelectionCanvas.GetComponentInChildren<Text>().text = "Player " + (_playerIndex + 1) + ", pick a token"; //Changes prompt text to prompt next player to choose token
+            }
+            else //Loads main game scene
+            {
+                SceneManager.LoadScene("GameScene");
+            }     
+        }
+
+        //Returns ever-changing player array so game can change player information
+        public Player[] GetPlayers()
+        {
+            return players;
+        }
+
         
     }
 }
--- Assets/Scripts/CharacterSelect/TokenPosition.cs
+++ Assets/Scripts/CharacterSelect/TokenPosition.cs
@@ -4,30 +4,7 @@ namespace PlayerSetupScripts
 {
     public class TokenPosition : MonoBehaviour
     {
-        private Vector3[] _tokenPositions = new Vector3[8];
-
         public GameObject[] tokens = new GameObject[8];
-        
-        // Start is called before the first frame update
-        void Start()
-        {
-            for (int n = 0; n < 4; n++)
-            {
-                Vector3 tempPosition = new Vector3( -768f + n * 512f, 450f, 0f);
-                _tokenPositions[n] = tempPosition;
-            }
-
-            for (int n = 4; n < 8; n++)
-            {
-                Vector3 tempPosition = new Vector3( -768f + (n - 4) * 512f, -100f, 0f);
-                _tokenPositions[n] = tempPosition;
-            }
-
-            for (int n = 0; n < 8; n++)
-            {
-                tokens[n].transform.position = _tokenPositions[n];
-            }
-        }
 
         public void SetTokenActives(bool active)
         {
